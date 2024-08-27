9936a538f79ed0a56e8e0bf94e217e36bdf21d99
hl <1799498990@qq.com>
2024-08-27 14:00:52 +0800

    new

--- app.vue
+++ app.vue
@@ -10,6 +10,6 @@ await api_index.init()
 useHead({
   title: "nuxt3-模板",
   meta: [{ name: "description", content: "描述" }],
-  link: [{ rel: "stylesheet", href: "/css/public.css" }],
+  link: [{ rel: "stylesheet", href: "/css/public.css" },{ rel: "stylesheet", href: "/font_css/result.css" }],
 });
 </script>
